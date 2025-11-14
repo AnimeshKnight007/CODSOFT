ca9b64022de5733740c3a7f3bdb4a1d919c775a4
AnimeshKnight007 <animesh.duttak@gmail.com>
2025-11-14 19:02:24 +0530

    Create Task2_Calculator.cpp

---
+++ Task2_Calculator.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+double a, b;
+char op;
+
+cout << "Enter first number: ";
+cin >> a;
+
+cout << "Enter operator (+ - * /): ";
+cin >> op;
+
+cout << "Enter second number: ";
+cin >> b;
+
+if (op == '+')
+cout << "Result = " << a + b << endl;
+else if (op == '-')
+cout << "Result = " << a - b << endl;
+else if (op == '*')
+cout << "Result = " << a * b << endl;
+else if (op == '/') {
+if (b == 0)
+cout << "Error: division by zero!" << endl;
+else
+cout << "Result = " << a / b << endl;
+}
+else
+cout << "Invalid operator!" << endl;
+
+return 0;
+}
